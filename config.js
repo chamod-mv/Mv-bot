09b2d9c7354bb5ad2d7494aabaf1d650eed12cc7
chamod-mv <cnwwsb@gmail.com>
2025-07-04 20:05:44 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "DId1QayS#szwnz9YoB7DyuRZ6_xC5UcK3kIvAEmQr6zefezlDnTc", //put your session id
+SESSION_ID: process.env.SESSION_ID || "w3VTwJbS#lQfFoAat_WRH7L1n4q5E8ssmKFH1tD8pLP286UMHZ8U", //put your session id
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/x5w7lg.jpeg",
 ALIVE_MSG: process.env.ALIVE_MSG || "*👋 Hello I am VVIP MOVIE BOT*",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",  //true or false
